134c93427c152ffb6e141f618a90fdf2edb48cfc
Mattia Moretta <mattia.moretta@gmail.com>
2025-10-08 07:31:49 +0200

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+const REPO_BASE = "/mba-project/";
+
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "./" : "/"
+  base: command === "serve" ? "/" : REPO_BASE
 }));
